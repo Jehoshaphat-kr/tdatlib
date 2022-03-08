f72e983ed8f5ad80d7b7ec8620e3d8d18ddb7016
TDAT <83201178+Jehoshaphat-kr@users.noreply.github.com>
2022-03-09 01:30:05 +0900

    Update update.py

--- update.py
+++ update.py
@@ -3,6 +3,5 @@ import tdatlib
 if __name__ == "__main__":
 
 
-    # market_map = tdatlib.treemap()
-    market_map = tdatlib.treemap(progress='print')
+    market_map = tdatlib.treemap()
     market_map.to_js()
