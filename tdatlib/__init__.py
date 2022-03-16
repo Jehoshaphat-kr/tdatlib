9cba4663f0bc6ddeb85f0e8a05f599ae03dd9eee
JEHYEUK-LEE <jhlee_0319@naver.com>
2022-03-16 18:49:40 +0900

    20220316 - auto mouse update

--- tdatlib/__init__.py
+++ tdatlib/__init__.py
@@ -3,4 +3,23 @@ from tdatlib.fetch.market import interface as market
 from tdatlib.fetch.stock import interface as stock
 from tdatlib.view.maps import treemap
 
+from datetime import datetime
+import pyautogui as pya
+import time as t
+def auto_mouse(t_gap:int=50, due:int=0):
+    """
+    :param t_gap: in format, int (seconds)
+    :param due: in format, %H%M
+    :return:
+    """
+    pya.FAILSAFE = False
+    width, height  = pya.size().width, pya.size().height
+
+    while int(datetime.now().strftime("%H%M")) <= due if due else True:
+        pya.click(int(width/2), 10)
+        t.sleep(t_gap)
+        pya.click(int(width / 2), int(height/2))
+        t.sleep(t_gap)
+    return
+
 
