c653ebe7b2e7f66c9556e3d71c37b3f71b2fc46f
Jehoshaphat-kr <jhlee_0319@naver.com>
2022-03-17 15:46:32 +0900

    20220317 auto_mouse Git Action Failed 수정

--- tdatlib/fetch/stock/api.py
+++ tdatlib/fetch/stock/api.py
@@ -1,10 +1,12 @@
 from .fnguide import *
 from .timeseries import *
+from ta import add_all_ta_features as taf
 
 
 class interface:
     __key = '종가'
-    __ohlcv, __rel, __perf = pd.DataFrame(), pd.DataFrame(), pd.DataFrame()
+    __ohlcv, __rel, __perf, __fiftytwo = pd.DataFrame(), pd.DataFrame(), pd.DataFrame(), pd.DataFrame()
+    __ta = pd.DataFrame()
     def __init__(self, ticker:str, name:str='Unknown', period:int=5):
         self.ticker, self.name, self.period = ticker, name, period
         return
@@ -15,6 +17,18 @@ class interface:
         self.__key = key
         self.__rel, self.__perf = pd.DataFrame(), pd.DataFrame()
         return
+
+    @property
+    def ta(self) -> pd.DataFrame:
+        """
+        Technical Analysis
+        https://technical-analysis-library-in-python.readthedocs.io/en/latest/ta.html#trend-indicators
+        """
+        if self.__ta.empty:
+            o, h, l, c, v = '시가', '고가', '저가', '종가', '거래량'
+            if len(self.ohlcv) > 20:
+                self.__ta = taf(self.ohlcv.copy(), open=o, close=c, low=l, high=h, volume=v)
+        return self.__ta
 
     @property
     def ohlcv(self) -> pd.DataFrame:
@@ -42,3 +56,52 @@ class interface:
         if self.__perf.empty:
             self.__perf = getPerformance(ohlcv=self.ohlcv, key=self.__key, name=self.name)
         return self.__perf
+
+    @property
+    def fiftytwo(self) -> pd.DataFrame:
+        """
+        52주 가격 및 대비 수익률
+        """
+        if self.__fiftytwo.empty:
+            self.__fiftytwo = getFiftyTwo(ohlcv=self.ohlcv, key=self.__key, name=self.name)
+        return self.__fiftytwo
+
+    @property
+    def bollinger(self) -> pd.DataFrame:
+        """
+        볼린저밴드(Bollinger Band)
+        """
+        return pd.concat(
+            objs={
+                'upper': self.ta.volatility_bbh, 'lower': self.ta.volatility_bbl, 'mid': self.ta.volatility_bbm,
+                'width': self.ta.volatility_bbw, 'signal': self.ta.volatility_bbp
+            }, axis=1
+        )
+
+    @property
+    def rsi(self) -> pd.Series:
+        """
+        RSI: Relative Strength Index 데이터프레임
+        :return:
+        """
+        return pd.concat(
+            objs={
+                'rsi':self.ta.momentum_rsi,
+                'stochastic': self.ta.momentum_stoch,
+                'stochastic-signal': self.ta.momentum_stoch.signal
+            }, axis=1
+        )
+
+    @property
+    def macd(self) -> pd.DataFrame:
+        """
+        MACD: Moving Average Convergence & Divergence 데이터프레임
+        :return:
+        """
+        return pd.concat(
+            objs={
+                'macd': self.ta.trend_macd,
+                'signal': self.ta.trend_macd_signal,
+                'histogram': self.ta.trend_macd_diff
+            }, axis=1
+        )
--- tester.py
+++ tester.py
@@ -1,10 +1,28 @@
+from tdatlib import market
 from pykrx import stock as krx
 from datetime import datetime, timedelta
 from pytz import timezone
 from tqdm import tqdm
 import pandas as pd
-import time
-from tdatlib import auto_mouse, market
+import time as t
+import pyautogui as pya
+
+
+def auto_mouse(t_gap:int=50, due:int=0):
+    """
+    :param t_gap: in format, int (seconds)
+    :param due: in format, %H%M
+    :return:
+    """
+    pya.FAILSAFE = False
+    width, height  = pya.size().width, pya.size().height
+
+    while int(datetime.now().strftime("%H%M")) <= due if due else True:
+        pya.click(int(width/2), 10)
+        t.sleep(t_gap)
+        pya.click(int(width / 2), int(height/2))
+        t.sleep(t_gap)
+    return
 
 
 if __name__ == "__main__":
