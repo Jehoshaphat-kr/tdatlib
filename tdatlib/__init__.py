bf280bc715d3cd64ac12660c82dee57209c48a96
Jehoshaphat-kr <jhlee_0319@naver.com>
2022-04-20 17:07:25 +0900

    20220420 - library manager update

--- tdatlib/__init__.py
+++ tdatlib/__init__.py
@@ -1,2 +1,49 @@
+"""
+Library Structure
+
+tdatlib
+  $ fetch
+    $ archive
+      * __init__.py
+      $ category
+        * __ETF.xlsx
+        * __THM.xlsx
+        * etf.csv
+        * theme.csv
+        * wi26.csv
+        * wics.csv
+      $ common
+        * backup-icm.csv
+        * backup-perf.csv
+        * icm.csv
+        * perf.csv
+      $ treemap
+        $ deploy
+          $ js
+            * /* Deploy java-scripts */
+          * suffix.js
+
+
+"""
 from tdatlib.interface.treemap import deploy as treemap_deploy
 from tdatlib.fetch.market import check_etf_handler
+import os
+
+
+def tree(path:str):
+    for root, dirs, files in os.walk(path):
+        level = root.replace(path, '').count(os.sep)
+        indent = '\t' * level
+        print('{}{}/'.format(indent, os.path.basename(root)))
+        sub_indent = '\t' * (level + 1)
+        for f in files:
+            if not f.endswith('.pyc') and not f.endswith('.xlsx'):
+                print('{}{}'.format(sub_indent, f))
+
+
+def implement(__dir):
+
+    return
+
+if __name__ == "__main__":
+    tree(os.path.dirname(__file__))
