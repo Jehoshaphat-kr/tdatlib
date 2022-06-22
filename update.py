5201f1787977cf1de72395fc259c6746821a771c
Jehoshaphat-kr <jhlee_0319@naver.com>
2022-06-22 16:15:30 +0900

    20220622 - Treemap Update

--- update.py
+++ update.py
@@ -1,8 +1,26 @@
 from tdatlib.dataset import market
 
 
+RUN_TREEMAP = True
+CHK_ETF     = False
+CHK_THEME   = False
+
 if __name__ == "__main__":
     myMarket = market.KR()
 
-    # myMarket.isetfokay()
-    myMarket.treemap_deploy()
+    if RUN_TREEMAP:
+        myMarket.treemap_deploy()
+
+    """
+    ETF 현황 체크
+    """
+    if CHK_ETF:
+        myMarket.isetfokay()
+
+    """
+    테마 항목 상폐 여부 확인
+    """
+    if CHK_THEME:
+        wics  = myMarket.wics
+        theme = myMarket.theme
+        print(theme[~theme.index.isin(wics.index)])
