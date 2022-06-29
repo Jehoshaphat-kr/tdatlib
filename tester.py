126ac7627ab7e515f45e621278d144baa985d950
Jehoshaphat-kr <jhlee_0319@naver.com>
2022-06-29 18:58:43 +0900

    20220628 - Treemap Update

--- tester.py
+++ tester.py
@@ -1,6 +1,6 @@
 from tdatlib.dataset import market
 from tdatlib.viewer import stock
-from tdatlib.tdef import *
+from tdatlib.tdef import labels
 
 DEBUG_MARKET  = False
 DEBUG_TREEMAP = False
@@ -18,17 +18,12 @@ if DEBUG_MARKET:
 
 if DEBUG_STOCK:
     viewer = stock.KR(
-        ticker=신한지주,
+        ticker=labels.우리금융지주,
         period=3
     )
     viewer.saveall()
 
 if DEBUG_GROUP:
-    # 은행 주식
-    tickers = [KB금융, 하나금융지주, 우리금융지주, 신한지주, 기업은행]
-
-
-    # tickers = ['000890', '186230', '149980', '024950', '276730', '033920']
-    for ticker in tickers:
+    for ticker in labels.banks:
         myStock = stock.KR(ticker=ticker, period=3)
         myStock.saveall()
