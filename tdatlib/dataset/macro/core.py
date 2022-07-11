2abc3acb456da1327da2d8bebaca50bda24d93f0
Jehoshaphat-kr <jhlee_0319@naver.com>
2022-07-11 19:02:14 +0900

    20220711 - Front Update

--- tdatlib/dataset/macro/core.py
+++ tdatlib/dataset/macro/core.py
@@ -1,7 +1,11 @@
 from pandas_datareader import get_data_fred as fred
 from datetime import datetime, timedelta
+from xml.etree.ElementTree import ElementTree, fromstring
 import pandas as pd
 import requests
+
+
+KEY = "CEW3KQU603E6GA8VX0O9"
 
 
 def fetch_fred(symbols:str or list, period:int = 10):
@@ -9,6 +13,56 @@ def fetch_fred(symbols:str or list, period:int = 10):
     start = today - timedelta(period * 365)
     end = today
     return fred(symbols=symbols, start=start, end=end)
+
+
+class fetch_ecos(object):
+
+    _exclude = ['row', 'P_STAT_CODE']
+    def __init__(self):
+        return
+
+    def _xml_to_df(self, url:str) -> pd.DataFrame:
+        resp = requests.get(url)
+        root = ElementTree(fromstring(resp.text)).getroot()
+
+        data = list()
+        for tag in root.findall('row'):
+            getter = dict()
+            for n, t in enumerate([inner for inner in tag.iter()]):
+                if t.tag in self._exclude:
+                    continue
+                getter.update({t.tag: t.text})
+            data.append(getter)
+
+        return pd.DataFrame(data=data) if data else pd.DataFrame()
+
+    @property
+    def codes(self) -> pd.DataFrame:
+        if not hasattr(self, '__codes'):
+            url = f"http://ecos.bok.or.kr/api/StatisticTableList/{KEY}/xml/kr/1/10000/"
+            df = self._xml_to_df(url=url)
+            df = df[df.SRCH_YN == 'Y'].copy()
+            df['STAT_NAME'] = df.STAT_NAME.apply(lambda x: x[x.find(' ') + 1:])
+            df = df.drop(columns='SRCH_YN')
+            df = df.rename(columns={'STAT_CODE':'코드', 'STAT_NAME':'지표명', 'CYCLE':'주기', 'ORG_NAME':'발행처'})
+            self.__setattr__('__codes', df)
+        return self.__getattribute__('__codes')
+
+    def fetch(self, codes:list or str, start_year:str='2015', end_year:str='2022'):
+        if type(codes) == str:
+            codes = [codes]
+
+        yy = start_year
+        ee = end_year
+        samples = self.codes[self.codes.코드.isin(codes)]
+        for code, label, cyc in zip(samples.코드, samples.지표명, samples.주기):
+            s = f'{yy}0101' if cyc == 'D' else f'{yy}01S1' if cyc == 'SM' else f'{yy}01' if cyc == 'M' else f'{yy}{cyc}1'
+            e = f'{ee}1230' if cyc == 'D' else f'{ee}12S1' if cyc == 'SM' else f'{ee}12' if cyc == 'M' else f'{ee}{cyc}12'
+            suffix = s[-4:] if len(s) == 8 else s[-2:]
+            url = f'http://ecos.bok.or.kr/api/StatisticSearch/{KEY}/xml/kr/1/100000/{code}/{cyc}/{s}/{e}'
+            df = self._xml_to_df(url=url)
+            df.to_csv(rf'./{code}_{label}.csv', index=False, encoding='euc-kr')
+
 
 
 def fetch_kr():
@@ -19,14 +73,29 @@ def fetch_kr():
 
 
 if __name__ == "__main__":
-
-    data = fetch_kr()
-    frm = pd.DataFrame(data['KeyStatisticList']['row'])
-    file = open('./raw.txt', 'w')
-    file.write(str(data))
-    file.close()
+    pd.set_option('display.expand_frame_repr', False)
+    # data = fetch_kr()
+    # frm = pd.DataFrame(data['KeyStatisticList']['row'])
+    # file = open('./raw.txt', 'w')
+    # file.write(str(data))
+    # file.close()
     # print(frm)
     # frm.to_csv('./test.csv', encoding='euc-kr', index=False)
+
+    ecos = fetch_ecos()
+    # print(ecos.codes)
+    # ecos.codes.to_csv(r'./test.csv', encoding='euc-kr')
+
+    ecos.fetch(
+        codes=[
+            '802Y001',
+            '901Y013',
+            '301Y017',
+            '731Y003',
+            '511Y003'
+        ]
+    )
+
 
     # from tdatlib.tdef import labels
     # from tdatlib.dataset import stock
