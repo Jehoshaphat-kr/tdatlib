e35a53f4895ba96114889b93e7008aab5c80f7a6
Jehoshaphat-kr <jhlee_0319@naver.com>
2022-07-22 16:05:50 +0900

    20220722 - Treemap Update - Front

--- tdatlib/dataset/macro/core.py
+++ tdatlib/dataset/macro/core.py
@@ -70,7 +70,68 @@ class fetch_ecos(object):
         rev.index = pd.to_datetime(rev.index)
         return rev
 
-    def fetch(self, codes:list or str, start_year:str='2015', end_year:str='2022'):
+    @property
+    def interest_rate(self) -> pd.DataFrame:
+        """
+        국내 금리
+        """
+        cols = {
+            "자금조정 예금금리"     : "예금금리",
+            "자금조정 대출금리"     : "대출금리",
+            "한국은행 기준금리"     : "기준금리",
+            "콜금리(1일, 전체거래)" : "콜금리1D",
+            "CD(91일)"              : "CD금리91D",
+            "CP(91일)"              : "CP금리91D",
+            "국고채(2년)"           : "국고채2Y",
+            "국고채(3년)"           : "국고채3Y",
+            "국고채(5년)"           : "국고채5Y",
+            "국고채(10년)"          : "국고채10Y",
+            "국고채(30년)"          : "국고채20Y",
+            "회사채(3년, AA-)"      : "회사채3YAA-",
+            "회사채(3년, BBB-)"     : "회사채3YBBB-",
+        }
+        raw = self.fetch(codes=['722Y001', '817Y002'])
+        rev = raw[list(cols.keys())].rename(columns=cols)
+        rev.index = pd.to_datetime(rev.index)
+        return rev
+
+    @property
+    def nsi(self) -> pd.DataFrame:
+        """
+        뉴스심리지수[한국은행]
+        한국은행은 경제분야 뉴스기사에 나타난 경제심리를 지수화한 뉴스심리지수(NSI)를 ECOS에 실험적으로 공개
+
+        - 포털사이트의 경제분야 뉴스기사를 기반으로 표본문장을 추출
+        - 각 문장에 나타난 긍정, 부정, 중립의 감성을 기계학습 방법으로 분류
+        - 긍정과 부정 문장수의 차이를 계산하여 지수화한 지표
+
+        * 지수 작성대상 표본은 ’05년 이후 50여개 언론사의 경제분야 뉴스기사 문장으로,
+          일별 10,000개의 표본문장을 무작위로 추출하였음.
+
+        * ’05.1.1~’22.1.31일중 일별 뉴스심리지수를 작성한 결과,
+          장기평균치 100을 기준으로 대체로 대칭적인 흐름을 보이는 가운데 경제심리의 변화를 신속하게 포착됨.
+        """
+        raw = self.fetch(codes='521Y001')
+        raw.index = pd.to_datetime(raw.index)
+        return raw
+
+    @property
+    def bca(self) -> pd.DataFrame:
+        """
+        경상 수지 Balance On Current Account
+        경상수지가 흑자라는 것은 상품과 서비스 등의 수출과 관련해 늘어나는 생산과 일자리가 수입으로 인해 줄어드는 것보다
+        크다는 것을 의미한다. 때문에 그만큼 국민의 소득 수준이 높아지고 고용이 확대되는 긍정적인 효과가 있다.
+
+        반대로 경상수지가 적자를 나타내는 경우 소득 수준이 낮아지고 실업이 늘어남과 동시에 외채가 늘어나면서 대외신인도가
+        하락하게 된다. 나아가 국내외 여건 악화로 자본유입이 급격히 둔화되거나 대규모 자본유출이 나타날 경우 외화 유동성
+        부족으로 외환위기에 이르게 될 가능성이 높아진다.
+        """
+        raw = self.fetch(codes='301Y017')
+        rev = raw['경상수지']
+        rev.index = pd.to_datetime(rev.index)
+        return rev
+
+    def fetch(self, codes:list or str, start_year:str='2015', end_year:str='2022') -> pd.DataFrame:
         if type(codes) == str:
             codes = [codes]
 
@@ -84,6 +145,8 @@ class fetch_ecos(object):
                 e = f'{ee}1230' if cyc == 'D' else f'{ee}12S1' if cyc == 'SM' else f'{ee}12' if cyc == 'M' else f'{ee}{cyc}12'
                 url = f'http://ecos.bok.or.kr/api/StatisticSearch/{KEY}/xml/kr/1/100000/{code}/{cyc}/{s}/{e}'
                 df = self._xml_to_df(url=url)
+                if cyc == 'M':
+                    df['TIME'] = df['TIME'] + '15'
 
                 _objs = list()
                 for item in df['ITEM_NAME1'].drop_duplicates(keep='first'):
@@ -113,20 +176,19 @@ if __name__ == "__main__":
     # print(ecos.codes)
     # print(ecos.krw_usd)
     # print(ecos.krw_cny)
+    # print(ecos.interest_rate)
+    # print(ecos.nsi)
+    print(ecos.bca)
 
     # ecos.codes.to_csv(r'./test.csv', encoding='euc-kr')
 
-    df = ecos.fetch(
-        codes=[
-            '817Y002'
-            # '802Y001',
-            # '901Y013',
-            # '301Y017',
-            # '511Y003'
-        ]
-    )
-    df.to_csv(r'./test.csv', encoding='euc-kr')
-    print(df)
+    # df = ecos.fetch(
+    #     codes=[
+    #
+    #     ]
+    # )
+    # df.to_csv(r'./test.csv', encoding='euc-kr')
+    # print(df)
 
 
     # from tdatlib.tdef import labels
