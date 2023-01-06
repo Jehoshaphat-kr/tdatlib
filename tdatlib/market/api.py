fff4a6d3f447002234ab526659fc4f84c90dbd3e
JEHYEUK-LEE <jhlee_0319@naver.com>
2023-01-06 15:39:35 +0900

    20230106 - Source Update

--- tdatlib/market/api.py
+++ tdatlib/market/api.py
@@ -62,7 +62,7 @@ class _market(object):
     def pd2js(self):
         td = krse.rdate
         js = os.path.join(os.path.dirname(__file__), f'archive/deploy/marketdata.js')
-        syntax = f'var trading_date = "({td[2:4]}.{td[4:6]}.{td[6:]} 종가 기준)";\n'
+        syntax = f'var trading_date = " ({td[2:4]}.{td[4:6]}.{td[6:]} 종가 기준)";\n'
 
         # proc = [('labels', self._labels), ('covers', self._covers), ('ids', self._ids), ('bar', self._bars)]
         proc = [('tdat_labels', self._labels), ('tdat_covers', self._covers), ('tdat_ids', self._ids)]
@@ -86,7 +86,7 @@ class _market(object):
         td = krse.rdate
         json = os.path.join(os.path.dirname(__file__), f'archive/deploy/marketdata.json')
 
-        syntax = "{\n" + f'  "tdat_td":"({td[2:4]}.{td[4:6]}.{td[6:]} 종가 기준)",\n'
+        syntax = "{\n" + f'  "tdat_td":" ({td[2:4]}.{td[4:6]}.{td[6:]} 종가 기준)",\n'
         proc = [('tdat_labels', self._labels), ('tdat_covers', self._covers), ('tdat_ids', self._ids)]
         for name, data in proc:
             syntax += '  "%s":{\n' % name
