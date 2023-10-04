8ed9dd3d25d65f8fe03681370eb6c4716be8ecc8
Jehoshaphat-kr <jhlee_0319@naver.com>
2023-10-04 15:42:26 +0900

    Treemap Update

--- update.py
+++ update.py
@@ -1,11 +1,14 @@
 from snob import market as treemap
-from snob.market.core import etf
 
 RUN_TREEMAP = True
 CHK_ETF     = False
 
 if __name__ == "__main__":
-    # etf.islatest()
+
+    if CHK_ETF:
+        from snob.market.core import etf
+        etf.islatest()
+
 
     new_treemap = treemap.marketmap
 
